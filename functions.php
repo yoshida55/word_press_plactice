6f0f15aa894923bd07488c50ee847a64caa09ccc
sensh <sensha5172@gmail.com>
2026-07-24 08:40:17 +0900

    update

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ function mystore_files()
     wp_enqueue_style('mystore-reset', get_template_directory_uri() . '/css/reset.css');
     wp_enqueue_style('mystore-main', get_stylesheet_uri()); // テーマ直下の style.css
     wp_enqueue_style('mystore-index', get_template_directory_uri() . '/css/index.css');
+    wp_enqueue_style('mystore-detail', get_template_directory_uri() . '/css/detail.css');
 
     // JS（最後の true = </body>直前で読む）
     wp_enqueue_script('mystore-index', get_template_directory_uri() . '/js/index.js', array(), false, true);
--- single.php
+++ single.php
@@ -1,11 +1,36 @@
 <?php get_header(); ?>
 
+
+<!-- メイン -->
 <main>
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <h1><?php the_title(); ?></h1>
-      <?php the_post_thumbnail('medium'); ?>
-      <?php the_content(); ?>
-  <?php endwhile; endif; ?>
+  <!-- @01 商品詳細 ここから -->
+  <section class="product_detail">
+
+
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <h2 class="product_name"><?php the_title(); ?></h2>
+
+        <!-- 画像と説明文【flex 横2列: 画像 + テキスト側】 -->
+        <div class="product_inner">
+          <!-- └ 画像（左） -->
+          <div class="product_detail_img">
+            <?php if ( has_post_thumbnail() ) : ?>
+              <?php the_post_thumbnail('medium', array('class' => 'product_img')); ?>
+            <?php endif; ?>
+          </div>
+          <!-- └ テキスト側（右） -->
+          <div class="product_detail_info">
+            <h2 class="product_detail_title">プロダクトタイトルプロダクトタイトル</h2>
+            <p class="product_detail_price">¥99,999 <br />+tax</p>
+            <p class="product_detail_description">説明説明説明説明説明説明</p>
+            <button class="product_detail_button">カートに入れる</button>
+          </div>
+        </div>
+
+    <?php endwhile; endif; ?>
+  </section>
+  <!-- @01 商品詳細 ここまで -->
 </main>
 
 <?php get_footer(); ?>
