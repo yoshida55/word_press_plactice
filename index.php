d3b38d4df287c190fc04c59f730e41aa4e4be821
sensh <sensha5172@gmail.com>
2026-07-24 07:46:17 +0900

    update

--- index.php
+++ index.php
@@ -20,11 +20,20 @@
             <p class="product_title"><?php the_title(); ?></p>
             <!-- 価格などのカスタムフィールドがある場合はここで出力 -->
             <p class="product_price">¥99,999 +tax</p>
+            <!-- <a class="product_link" href="detail.html">View Detail</a> -->
+            <a class="product_link" href="<?php the_permalink(); ?>">View Detail</a>
           </li>
       <?php endwhile; endif; ?>
     </ul>
 
     <!-- 
+
+          <li class="product_item">
+            <img class="product_img" src="img/item2.jpg" alt="椅子" />
+            <p class="product_title">プロダクトタイトルプロダクトタイトル</p>
+            <p class="product_price">¥99,999 +tax</p>
+            <a class="product_link" href="detail.html">View Detail</a>
+          </li>
 
       <li class="product_item">
         <img class="product_img" src="<?php echo esc_url(get_template_directory_uri() . '/img/item2.jpg'); ?>" alt="椅子" />
@@ -66,16 +75,15 @@
         <p class="product_title">プロダクトタイトルプロダクトタイトル</p>
         <p class="product_price">¥99,999 +tax</p>
       </li> -->
-  </ul>
 
-  <div id="product_link">
-    <!-- 戻るボタン -->
-    <a class="product_back" href="#">戻る</a>
-    <!-- もっと見るリンク -->
-    <a class="product_more" href="#">View More</a>
-  </div>
+    <div id="product_link">
+      <!-- 戻るボタン -->
+      <a class="product_back" href="#">戻る</a>
+      <!-- もっと見るリンク -->
+      <a class="product_more" href="#">View More</a>
+    </div>
 
-</section>
+  </section>
 </main>
 
 <!-- フッター -->
