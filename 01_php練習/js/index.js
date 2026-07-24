48f9088d449ab9885b0f110a1b625ebac031228f
takahashi kenzo <linkworks.sendai04@gmail.com>
2026-07-24 13:40:57 +0900

    update

--- 01_php練習/js/index.js
+++
@@ -1,8 +0,0 @@
-// ビューモアがおされたときshowをつける
-const viewMoreButton = document.querySelector(".product_more");
-const productItems = document.querySelectorAll(".product_item");
-
-viewMoreButton.addEventListener("click", () => {
-  productItems.forEach((item) => item.classList.add("show"));
-  viewMoreButton.style.display = "none"; // ボタンを非表示にする
-});
